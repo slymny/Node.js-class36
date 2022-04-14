2e3b72901748e7eb77ca2e96eda9dcb85cb09110
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-04-14 16:25:57 +0200

    homework is improved

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -3,17 +3,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 })
 
-app.use(express.json());
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
   if(!cityName) {
-    res.status(400).send('Please enter a valid city name.');
+    res.status(400).json({msg: 'Please enter a valid city name.'});
   } else {
-    res.status(200).json(cityName);
+    res.status(200).json({cityName});
   }
 })
 
